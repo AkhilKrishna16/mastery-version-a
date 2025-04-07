6853b43027ac3855fc7c22171cfe1d153ee6c140
AkhilKrishna16 <akhil200@cs.utexas.edu>
2025-04-06 21:30:32 -0500

    Fucl

--- reader.h
+++ reader.h
@@ -4,7 +4,8 @@
 
 #define MAX_LINE_LENGTH 512
 
-typedef enum {
+typedef enum
+{
     TOK_NUM,
     TOK_ADD,
     TOK_MULT,
@@ -12,17 +13,19 @@ typedef enum {
     TOK_DIV
 } TokenType;
 
-struct Token {
-    int val; //undefined if not TOK_NUM.
+struct Token
+{
+    int val; // undefined if not TOK_NUM.
     TokenType tok_type;
-    struct Token* next; //next token on the stack
+    struct Token *next; // next token on the stack
 };
 
-struct Reader {
-    struct Token* token; //the current token
-    char* save_ptr; //the pointer to the next token to be read
-    bool had_error; //set to true if the calculator can not return an answer.
+struct Reader
+{
+    struct Token *token; // the current token
+    char *save_ptr;      // the pointer to the next token to be read
+    bool had_error;      // set to true if the calculator can not return an answer.
 };
 
-char* init_reader(struct Reader* reader, char* filename);
-bool advance(struct Reader* reader);
+char *init_reader(struct Reader *reader, char *filename);
+bool advance(struct Reader *reader);
